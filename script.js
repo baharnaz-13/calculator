42c84d0c7f9313d685cc312869f0fc55476d2172
Elina Zandi <93781927+baharnaz-13@users.noreply.github.com>
2025-09-01 11:55:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function calculate() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let operator = document.getElementById("operator").value;
+    let result;
+
+    if(isNaN(num1) || isNaN(num2)) {
+        result = "please enter two number ";
+    } else if(operator === "+") {
+        result = num1 + num2;
+    } else if(operator === "-") {
+        result = num1 - num2;
+    } else if(operator === "*") {
+        result = num1 * num2;
+    } else if(operator === "/") {
+        if (num2 === 0){
+            result = "Divided by zero is not allowed!";
+        } else {
+        result = num1 / num2;
+        }
+    } else {
+        result = "Invalid";
+    }
+
+    document.getElementById("result").innerText = "result:" + result;
+}
